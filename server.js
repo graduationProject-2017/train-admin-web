514acb78f77e6ca23c0f05043578f8ed1fb94396
RuyuYou <727809166@qq.com>
2017-04-07 20:16:20 +0800

    add fixture

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import webpack from 'webpack';
 import webpackConfig from './webpack.config';
+const mongoose = require('mongoose');
+const config = require('config');
+const bodyParser = require('body-parser');
+
+mongoose.connect(config.get('mongoUri'));
 
 const app = express();
 const compiler = webpack(webpackConfig);
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: webpackConfig.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
-
-
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -19,6 +24,6 @@ app.get('/', (res, req)=> {
   req.send('abc');
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("server started at http://localhost:3000");
 });
